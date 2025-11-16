8c25276c24057fa87fcdaa2c0c5dfbdb03f8f0be
Dharavath Vasu Naik <168115062+iamvasu18@users.noreply.github.com>
2025-11-16 23:00:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function volume_sphere(e) {
   const radius=document.getElementById("radius").value;
   const r = parseFloat(radius); 
   const volume = (4/3) * Math.PI * Math.pow(r, 3);
-  document.getElementById("volume").value = volume
+  document.getElementById("volume").value = volume.toFixed(4)
   
   
 } 
